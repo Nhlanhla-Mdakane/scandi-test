eeb1fd9c058827f387388b8947f0e8c2a174ce1b
Nhlanhla Mdakane <64080729+Nhlanhla-Mdakane@users.noreply.github.com>
2022-05-29 16:31:15 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,5 +1,3 @@
-
-
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Credentials: true');
@@ -7,54 +5,64 @@ header('Access-Control-Allow-Methods:POST,GET,PUT,DELETE');
 header('Access-Control-Allow-Headers: content-type or other');
 header('Content-Type: application/json');
 
-$servername = "localhost";
-$username   = "root";
-$password   = "password";
-$dbname     = "productsdb";
+include ("DbConnect.php");
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
+if ($conn->connect_error)
+{
     die("Connection failed: " . $conn->connect_error);
 }
 
 //Add product
-if(isset($_POST['sku']))
+if (isset($_POST['sku']))
 {
-     $sql = "insert into products (id ,sku , name ,price , type ,attributes)
+    $sql = "insert into products (id ,sku , name ,price , type ,attributes)
 
-     VALUES (null,'".$_POST['sku']."', '".$_POST['name']."', '".$_POST['price']."', '".$_POST['type']."'
-       , '".$_POST['attributes']."')";
+     VALUES (null,'" . $_POST['sku'] . "', '" . $_POST['name'] . "', '" . $_POST['price'] . "', '" . $_POST['type'] . "'
+       , '" . $_POST['attributes'] . "')";
 
-
-    if (mysqli_query($conn,$sql)) {
-    $data = array("data" => "You Data added successfully");
+    if (mysqli_query($conn, $sql))
+    {
+        $data = array(
+            "data" => "You Data added successfully"
+        );
         echo json_encode($data);
-    } else {
+    }
+    else
+    {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
 }
 //Delete user
-elseif(isset($_POST['deleteid']))
+elseif (isset($_POST['deleteid']))
 {
-    $sql = mysqli_query($conn, "DELETE from products where id =".$_POST['deleteid']);
-    if ($sql) {
+    $sql = mysqli_query($conn, "DELETE from products where id =" . $_POST['deleteid']);
+    if ($sql)
+    {
 
         //Success Message
-        $data = array("data" => "Record deleted successfully");
+        $data = array(
+            "data" => "Record deleted successfully"
+        );
         echo json_encode($data);
-      } else {
+    }
+    else
+    {
 
-        $data = array("data" =>"Error deleting record: " . mysqli_error($conn));
+        $data = array(
+            "data" => "Error deleting record: " . mysqli_error($conn)
+        );
         echo json_encode($data);
-      }
+    }
 }
 else
 {
     //get all users details
     $trp = mysqli_query($conn, "SELECT * from products");
     $rows = array();
-    while($r = mysqli_fetch_assoc($trp)) {
+    while ($r = mysqli_fetch_assoc($trp))
+    {
         $rows[] = $r;
     }
     print json_encode($rows);
